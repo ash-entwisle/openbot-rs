4fa0a262c76844d0d726be87713a85687eac3fea
Ash Entwisle <ash.entwisle@protonmail.com>
2023-08-02 22:22:53 +0000

    fixed my donegoofery lmao
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- install/install.sh
+++ install/install.sh
@@ -5,23 +5,23 @@ cd $(pwd)/openbot
 
 # Get Config File
 echo "Download Config File"
-curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/install/config.sh -o config.sh
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/install/config.toml > config.toml
 
 # Get Template Env File
 echo "Download Template Env File"
-curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/install/temp.env -o .env
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/install/temp.env > .env
 
 # Get Docker Compose 
 echo "Download Docker Compose File"
-curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/install/docker-compose.yaml -o docker-compose.yaml
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/install/docker-compose.yaml > docker-compose.yaml
 
 # Get License File
 echo "Download License File"
-curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/LICENSE -o LICENSE
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/LICENSE > LICENSE
 
 # Get Readme File
 echo "Download Readme File"
-curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/README.md -o README.md
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot-rs/main/README.md > README.md
 
 # Done !!
 echo "Done !!"
